edae4e86d7b474e7959489f2a10feac243368e79
Unknown <c.yhkim17@gmail.com>
2018-01-20 18:29:19 +1300

    Commit

---
+++ src/app/app-navbar/app-navbar.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-app-navbar',
+  templateUrl: './app-navbar.component.html',
+  styleUrls: ['./app-navbar.component.css']
+})
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: 'app-navbar.component.html',
+  styles: []
+})
+export class AppNavbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    
+  }
+
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyCKW0_DF6G1JT5EHsuAx2CJf2_EKz-eka4",
+    authDomain: "meta-matrix-128809.firebaseapp.com",
+    databaseURL: "https://meta-matrix-128809.firebaseio.com",
+    projectId: "meta-matrix-128809",
+    storageBucket: "meta-matrix-128809.appspot.com",
+    messagingSenderId: "340770722473"
+  }
+};
